9a6effbe81c252428a8a20587977f36027ef5a10
LlamaLad7 <l3gomindstorms@gmail.com>
2022-06-30 20:54:14 +0100

    delay initialization of config until it is first referenced. fixes https://github.com/MoriyaShiine/enchancement/issues/26

--- src/main/java/moriyashiine/enchancement/common/ModConfig.java
+++ src/main/java/moriyashiine/enchancement/common/ModConfig.java
@@ -66,4 +66,8 @@ public class ModConfig implements ConfigData {
 		}
 		return allowedEnchantments.contains(string);
 	}
+
+	static {
+		MidnightConfig.init(Enchancement.MOD_ID, ModConfig.class);
+	}
 }
